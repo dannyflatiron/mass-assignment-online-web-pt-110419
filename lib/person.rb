84718da037311879fbeaacc7e9984ba8eb8b5c19
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 01:36:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_width, :pant_length
 
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
+    binding.pry
   end
 
 end
