473e3d92168e057c16b6510cf841a83a1a5890b0
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 01:38:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,11 @@ class Person
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
+    # attributes is a hash
+    # iterate over each key, value pair in the attributes hash.
+    # the key is the setter method
+    # the value is the argument passed into the setter method
+    # .send calls the method with the passed argument
     # binding.pry
   end
 
